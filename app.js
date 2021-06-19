0fbb566f39a9fc9e8c310e4f40112362fd0d758e
migue-prog <miguel_ed_sand@hotmail.com>
2021-06-19 03:13:11 -0300

    creacion formulario de registro

--- app.js
+++ app.js
@@ -14,3 +14,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+
